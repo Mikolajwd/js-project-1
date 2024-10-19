2426c50569b04e9a26609d23e012715a64da7d63
NikoWD <mikolaj.wd0821@gmail.com>
2024-10-20 00:17:32 +0200

    update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const arrowBtn = document.querySelector('.arrow')
 const arrowIcon = document.querySelector('.fas')
 const img = document.querySelector('.item1')
 const img2 = document.querySelector('.item2')
+const btn = document.querySelector('.btn')
 
 const showImg = () => {
     img.classList.toggle('hide')  
@@ -23,4 +24,18 @@ const combinatedFunction = () => {
     addRotate()
 }
 
+
+const addDiv = ( )=> {
+console.log('hello world');
+const newDiv = document.createElement('div')
+const newSpan = document.createElement('span')
+const newPar = document.createElement('par')
+newPar.textContent = 'Hi to jestem ja'
+document.body.append(newDiv)
+newDiv.append(newSpan)
+newSpan.append(newPar)
+
+}
+
 arrowBtn.addEventListener('click', combinatedFunction)
+btn.addEventListener('click', addDiv)
