d5a1d6632e07ce0c22de91690cd743365fcb431b
NikoWD <mikolaj.wd0821@gmail.com>
2024-10-21 00:20:52 +0200

    retry project

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const arrowIcon = document.querySelector('.fas')
 const img = document.querySelector('.item1')
 const img2 = document.querySelector('.item2')
 const btn = document.querySelector('.btn')
+const btn2 = document.querySelector('.btn2')
+const btn3 = document.querySelector('.btn3')
+const btn4 = document.querySelector('.btn4')
 
 const showImg = () => {
     img.classList.toggle('hide')  
@@ -37,5 +40,18 @@ newSpan.append(newPar)
 
 }
 
+let zmienna = null
+
+console.log(zmienna);
+const action = (e) => {
+        console.log(e.target.textContent);
+        zmienna = e.target.textContent
+        console.log(`zmienna została zmieniona na wartość ${zmienna}`);
+}
+
 arrowBtn.addEventListener('click', combinatedFunction)
 btn.addEventListener('click', addDiv)
+btn2.addEventListener('click', action)
+btn3.addEventListener('click', action)
+btn4.addEventListener('click', action)
+
